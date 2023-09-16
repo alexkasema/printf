b2946caa96e69477f36ccd17c23b203acab85a90
patrickmuchiri <pmuchirig@gmail.com>
2023-09-16 06:58:35 -0700

    created functions to handle the character(s) arguments

--- main.h
+++ main.h
@@ -10,7 +10,9 @@ typedef struct symbols{
 } symbols;
 
 int _printf(const char *format, ...);
-
+int _putchar(char c);
+int _char(va_list args);
+int _string(va_list args);
 
 #endif
 
---
+++ print_string_s.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _char - a function that prints a character.
+ * @args: list of arguments.
+ * Return: character printed.
+ */
+
+int _char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (_putchar(c));
+}
+
+/**
+ * _string - a function that prints a string.
+ * @args: list of arguments.
+ * Return: number of characters printed.
+ */
+
+int _string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+
+	return (i);
+}
