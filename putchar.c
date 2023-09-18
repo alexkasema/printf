64e5584a4f6a76a2eae2fc6c741851b59421fc52
patrickmuchiri <pmuchirig@gmail.com>
2023-09-18 11:13:53 -0700

    a function that optimizes the number of writes

--- putchar.c
+++ putchar.c
@@ -7,7 +7,23 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[1024];
+	int bi = 0;
+
+	if (bi == 1024)
+	{
+		return (write(1, &c, 1));
+		bi = 0;
+	}
+	buffer[bi++] = c;
+
+	if (bi > 0)
+	{
+		write(1, buffer, bi);
+		bi = 0;
+	}
+	return (1);
 }
